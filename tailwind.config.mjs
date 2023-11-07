b7c9f568255ba760d6088be69e4a9e8cab492c3e
jerobejrobinson <jerobejrobinson@gmail.com>
2023-11-07 09:37:20 -0600

    compressed and updated most images to webp

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,11 +4,11 @@ export default {
 	theme: {
 		extend: {
 			backgroundImage: {
-				'hero-bg-mobile': "url('/images/hero/mobile/background.png')",
-				'hero-bg-laptop': "url('/images/hero/laptop/background.png')",
-				'about-repeat-logo': 'url(/light-mo-money-taxes-laptop-alt-logo.png)',
-				'optin-small': 'url(/images/optIn/large/email-opt-in-background-large.png)',
-				'optin-large': 'url(/images/optIn/small/email-opt-in-background-mobile.png)'
+				'hero-bg-mobile': "url('/images/hero/mobile/background.webp')",
+				'hero-bg-laptop': "url('/images/hero/laptop/background.webp')",
+				'about-repeat-logo': 'url(/light-mo-money-taxes-laptop-alt-logo-1.webp)',
+				'optin-small': 'url(/images/optIn/large/email-opt-in-background-large.webp)',
+				'optin-large': 'url(/images/optIn/small/email-opt-in-background-mobile.webp)'
 			},
 			colors: {
 				'main-green': '#2DA75A',
